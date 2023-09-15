42812f49cd1aa334c4ee1d19fa6ff5c09c9978ae
Nikki <dominiquelcruz@gmail.com>
2023-09-15 00:12:01 -0700

    database code added and data from data.sql added to database

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 /** BizTime express application. */
 
-
 const express = require("express");
 
 const app = express();
 const ExpressError = require("./expressError")
 
 app.use(express.json());
-
 
 /** 404 handler */
 
